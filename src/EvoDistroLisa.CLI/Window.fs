32b2e7463449326a9b6f41266fcfd35e3c5de849
Milosz Krajewski <miloszkrajewski@o2.pl>
2015-09-11 00:43:33 +0100

    working on simple UI

---
+++ src/EvoDistroLisa.CLI/Window.fs
@@ -0,0 +1,26 @@
+﻿namespace EvoDistroLisa.CLI
+
+module UI = 
+    open System.Windows
+    open System.Threading
+
+    let applicationRef: Application ref = ref null
+
+    let startup () = 
+        let signal = new ManualResetEventSlim()
+        let thread = Thread((fun () ->
+            let app = new Application()
+            applicationRef.Value <- app
+            app.ShutdownMode <- ShutdownMode.OnExplicitShutdown
+            signal.Set()
+            app.Run() |> ignore
+        ), IsBackground = true)
+        thread.SetApartmentState(ApartmentState.STA)
+        thread.Start()
+        signal.Wait()
+
+    let shutdown () = 
+        applicationRef.Value.Shutdown()
+
+    let invoke func = 
+        applicationRef.Value.Dispatcher.Invoke(func)
---
+++ src/EvoDistroLisa.UI/ImageViewer.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Windows.Media.Imaging;
+
+namespace EvoDistroLisa.UI
+{
+	/// <summary>
+	/// Interaction logic for ImageViewer.xaml
+	/// </summary>
+	public partial class ImageViewer: INotifyPropertyChanged
+	{
+		private BitmapSource _image;
+
+		public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+		public ImageViewer()
+		{
+			InitializeComponent();
+		}
+
+		public BitmapSource Image
+		{
+			get { return _image; }
+			set
+			{
+				_image = value;
+				PropertyChanged(this, new PropertyChangedEventArgs("Image"));
+			}
+		}
+	}
+}
