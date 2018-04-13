100c8c472687cc48a17d5440a780d810cf8e0264
Milosz Krajewski <milosz.krajewski@getbusy.com>
2018-04-13 11:25:01 +0100

    paket/fake cleanup

--- src/fake.cmd
+++ src/fake.cmd
@@ -1,3 +1,15 @@
 @echo off
-call %~dp0\paket.cmd restore
-%~dp0\packages\FAKE\tools\FAKE.exe build.fsx %*
+
+setlocal
+set target=%~dp0\.fake
+set fake=%target%\fake.exe
+
+if not exist %fake% (
+    rmdir /q /s %temp%\nuget\fake 2> nul
+    nuget install -out %temp%\nuget -excludeversion fake
+    xcopy %temp%\nuget\fake\tools\* %target%\
+	rmdir /q /s %temp%\nuget\fake 2> nul
+)
+
+%fake% %*
+endlocal
